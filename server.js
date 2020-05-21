6dcafb777553d14382a9ab0a57e34b421c0a3b49
george-g <georgegombert@gmail.com>
2020-05-21 10:00:16 -0600

    database connection moved to connection.js file, added dependancies to orm.js, removed connection from server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mysql = require('mysql');
 // const path = require('path');
 
 const app = express();
@@ -8,25 +7,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static("./public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
-
-connection.connect(function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 app.get('/', (req, res) => {
   res.render("index");
