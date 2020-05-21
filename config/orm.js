b541e416bb7dcd18304f089adda4122e974c8bb3
george-g <georgegombert@gmail.com>
2020-05-21 10:20:30 -0600

    orm now includes select insert and update function

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,24 @@
 const connection = require('./connection');
+
+let orm = {
+  selectAll: (table) => {
+    connection.query("SELECT * FROM ??", [table], (err, result) =>{
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: (table,burgerName) => {
+    connection.query("INSERT INTO ?? VALUES ??", [table, burgerName], (err, result) =>{
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne: (table, burgerId, eatStatus) => {
+    connection.query("UPDATE ?? SET devoured = ? WHERE id = ? ", [table, burgerId, eatStatus], (err, result) =>{
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+};
+
+module.exports = orm;
