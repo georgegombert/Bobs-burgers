e6f2031ff26d15bf64cafbcb57f1fea824fe5e6e
george-g <georgegombert@gmail.com>
2020-05-21 11:12:24 -0600

    orm tested and query bugs fixed for insert

--- config/orm.js
+++ config/orm.js
@@ -7,14 +7,14 @@ let orm = {
       cb(result);
     });
   },
-  insertOne: (table,burgerName, cb) => {
-    connection.query("INSERT INTO ?? VALUES ??", [table, burgerName], (err, result) =>{
+  insertOne: (table, colName, burgerName, cb) => {
+    connection.query("INSERT INTO ?? (??) VALUES (?);", [table, colName, burgerName], (err, result) =>{
       if (err) throw err;
       cb(result);
     });
   },
   updateOne: (table, burgerId, eatStatus, cb) => {
-    connection.query("UPDATE ?? SET devoured = ? WHERE id = ? ", [table, burgerId, eatStatus], (err, result) =>{
+    connection.query("UPDATE ?? SET devoured = ?? WHERE id = ? ", [table, eatStatus, burgerId], (err, result) =>{
       if (err) throw err;
       cb(result);
     });
