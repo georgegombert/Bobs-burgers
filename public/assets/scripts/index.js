809bc0f0b023f74e370e26a58eb74b974cbe56b3
george-g <georgegombert@gmail.com>
2020-05-27 11:59:58 -0600

    onclick events working for printed burger list. Burgers now have id dataset

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,13 +1,21 @@
-function newBurger() {
-  console.log("clicked");
-  event.preventDefault();
-
-  $.ajax({
-    url: "/api/burgers",
-    type: "POST"
+$(document).ready(() => {
+  
+  function newBurger() {
+    console.log("clicked");
+    event.preventDefault();
+    
+    $.ajax({
+      url: "/api/burgers",
+      type: "POST"
     })
     .then(() => {
       console.log ("worked");
       location.reload();
     });
-};
+  };
+  
+  $("#burgerList").click((event) => {
+    console.log(event.target.dataset.id);
+  });
+
+}); // end doc.ready
